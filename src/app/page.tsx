4b95280b566ade3858d543709a6b2d0f947a0ab7
Gang Zhang <gzhangx@hotmail.com>
2025-01-29 14:39:49 -0500

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+'use client'
 import Image from "next/image";
+import { util } from '@gzhangx/googleapi'
+import { useEffect } from "react";
 
 export default function Home() {
+  function doReq() {
+    util.doHttpRequest({
+      method: 'POST',
+      url: 'http://192.168.0.40:8001/sql',
+      data: {
+        "table": "trafficLog",
+        "groupAt": "day",
+        "where": [
+          //["day", "28", "29"]
+        ]
+      }
+    }).then(res => {
+      console.log(res.data)
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+  useEffect(() => {
+    doReq();
+  }, ['once'])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <button onClick={()=>doReq()}>Test</button>
         <Image
           className="dark:invert"
           src="/next.svg"
